2e0f39656c298360fb68559b73480748c901d8b9
Michel Costa <mikelfcosta@live.com>
2020-04-21 22:35:57 -0600

    Created PassButtonComponent and its story

---
+++ src/components/Pass/Pass.story.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {select, withKnobs} from "@storybook/addon-knobs";
+
+import {Pass, PassButtonStates} from "./";
+
+const STATES: Record<string, PassButtonStates> = {
+  CanPass: "CAN_PASS",
+  Passed: "PASSED",
+  CannotPass: "CANNOT_PASS",
+};
+
+export function PassStory() {
+  const statesKnob = select("State", STATES, "CAN_PASS");
+
+  return <Pass state={statesKnob}/>;
+}
+
+export default {
+  title: "MinimalSolo/Components/Game",
+  component: PassStory,
+  decorators: [withKnobs],
+};
---
+++ src/components/Pass/styles.ts
@@ -0,0 +1,33 @@
+import styled, { css } from "styled-components";
+
+export type PassButtonProps = { isActive: boolean; isPassable: boolean };
+export const Wrapper = styled.button<PassButtonProps>`
+  background-color: ${(props) => props.theme.colors.blackest};
+  border-radius: ${(props) => props.theme.radius.base}px;
+  border: 1px solid ${(props) => props.theme.colors.blackShadow};
+  box-shadow: ${(props) => props.theme.shadows.lighter};
+  box-sizing: border-box;
+  height: 156px;
+  outline: none;
+  position: relative;
+  transition: all 300ms ease-in-out;
+  width: 104px;
+
+  ${(props) =>
+    props.isActive &&
+    css`
+      background: ${props.theme.colors.greenBase};
+      border-color: ${props.theme.colors.greenShadow};
+    `}
+
+  ${(props) =>
+    props.isPassable &&
+    css`
+      cursor: pointer;
+
+      :hover {
+        background: ${props.theme.colors.blueBase};
+        border-color: ${props.theme.colors.blueShadow};
+      }
+    `}
+`;
