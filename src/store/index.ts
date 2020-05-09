e7bfc17b1cfad295f94f69bc6ac809abcd71c126
David Nascimento <davidpn.11@gmail.com>
2020-05-09 13:01:22 -0600

    remove old store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { loadState, saveState } from './persistStore';
 
 import { rootReducer } from './rootReducer';
 
 const store = createStore(
   rootReducer /* preloadedState, */,
-  loadState(),
   compose(
     applyMiddleware(thunk),
     //@ts-ignore
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   ),
 );
-
-store.subscribe(() => {
-  saveState(store.getState());
-});
 
 export default store;
