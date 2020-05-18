64ce91b6667d937933486b1283d7dc9bcf454dc6
Michel Costa <mikelfcosta@live.com>
2020-05-18 12:37:34 -0600

    Changed up some button styles

--- src/views/Lobby/components/JoinModal/index.tsx
+++ src/views/Lobby/components/JoinModal/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Field, Form, Formik } from 'formik';
+import { useHistory } from 'react-router-dom';
 
 import { Backdrop, ModalCard, ModalWrapper, ModalContent, ModalFooter, ModalTitle } from './styles';
 import { getAllPlayers } from '../../../../store/session/selectors';
 import { getPlayerId } from '../../../../store/playerHand/selector';
 import { TextField } from '../../../../components/TextField';
-import { unitJSX } from '../../../../utils/unit';
 import { Button } from '../../../../components/Button';
+import { unitJSX } from '../../../../utils/unit';
 
 export function JoinModal() {
+  const history = useHistory();
   const players = useSelector(getAllPlayers);
   const currentPlayerId = useSelector(getPlayerId);
   const isPlaying = Object.keys(players).some(playerId => playerId === currentPlayerId);
@@ -37,7 +39,9 @@ export function JoinModal() {
             </Form>
           </ModalContent>
           <ModalFooter>
-            <Button variant="secondary">Cancel</Button>
+            <Button variant="cancel" onClick={() => history.push('/')}>
+              Cancel
+            </Button>
             <Button>Join</Button>
           </ModalFooter>
         </ModalCard>
