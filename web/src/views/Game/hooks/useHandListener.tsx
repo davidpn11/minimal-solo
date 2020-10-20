f72ef0075b36b921e4b1f5c98d1f971342749a4d
David Nascimento (Skip) <david.nascimento@skipthedishes.ca>
2020-10-19 20:36:43 -0600

    remove imports

--- web/src/views/Game/hooks/useHandListener.tsx
+++ web/src/views/Game/hooks/useHandListener.tsx
@@ -16,16 +16,8 @@ export function useHandListener() {
   const player = useSelector(getPlayerValue);
   const playerHand = useSelector(getPlayerHandIds);
   const playerActions = useSelector(getPlayerActions);
-  const lastPlayPosition = useSelector(getLastPlayPosition);
-  const currentCard = useSelector(getCurrentCard);
-  const currentPlayer = useSelector(getCurrentPlayer);
-  const nextPlayer = useSelector(getNextPlayer);
   const currentSession = useSelector(getSession);
-  const currentSessionPlayer = useSelector(getCurrentSessionPlayerValue);
-  const currentSessionPlayerWithId = { ...currentSessionPlayer, id: player.id };
-  const isYourTurn = useMemo<boolean>(() => player.id === currentPlayer, [player, currentPlayer]);
   const [hasListener, setHasListener] = useState<boolean>(false);
-  const [hasDrawed, setHasDrawed] = useState<boolean>(false);
   const dispatch = useDispatch();
 
   //Player hand listener
