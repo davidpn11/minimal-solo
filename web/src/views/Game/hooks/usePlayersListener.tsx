c03ab452f79f47224640cf21c4726364d3e40a90
Michel Costa <mikelfcosta@live.com>
2020-11-15 20:42:57 -0600

    Updated and fixed some issues

--- web/src/views/Game/hooks/usePlayersListener.tsx
+++ web/src/views/Game/hooks/usePlayersListener.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { normalizeQuery } from '@mikelfcosta/solo-lib/lib/utils/firebase';
+
 import { getSessionRef } from '../../../api/firebase';
 import { setPlayers } from '../../../store/session/actions';
 import { getSession } from '../../../store/session/selectors';
